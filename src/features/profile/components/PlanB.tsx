34f062c81a4926eccce6ab0bd3cb78eb97f1f1ba
Maxine Afable <maxineafable1@gmail.com>
2025-01-08 19:18:14 +0800

    fix eslint run build

--- src/features/profile/components/PlanB.tsx
+++ src/features/profile/components/PlanB.tsx
@@ -10,7 +10,7 @@ export default function PlanB({ planB }: Props) {
       <div className="space-y-4 bg-black/50 p-8 rounded-lg">
         <div className="text-center space-y-2">
           <p className="font-semibold text-2xl">
-            What are your plans if your plans on pre-med course didn't work
+            What are your plans if your plans on pre-med course didn&apos;t work
             &#40;during or after you graduate?&#41;
           </p>
         </div>
