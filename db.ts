dc7a66ff13fff0fc646c646d85a3c45c59568140
Maxine Afable <maxineafable1@gmail.com>
2025-11-19 07:46:28 +0800

    fix prod db path and permission

--- db.ts
+++ db.ts
@@ -1,17 +1,18 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
-// import sqlite from 'better-sqlite3'
+import sqlite from 'better-sqlite3'
 import * as schema from './db-schema';
-import Database from 'better-sqlite3';
+// import Database from 'better-sqlite3';
 
 // export const db = drizzle(process.env.DATABASE_URL!);
+const url = `/data/${process.env.DATABASE_URL}`
 
-const sqlite = new Database(process.env.DATABASE_URL)
+// const sqlite = new Database(process.env.DATABASE_URL)
 
-// const client = sqlite(process.env.DATABASE_URL!)
+const client = sqlite(url)
 // client.pragma('journal_mode = WAL')
 
 export const db = drizzle({
-  client: sqlite,
+  client,
   // client,
   schema,
 })
