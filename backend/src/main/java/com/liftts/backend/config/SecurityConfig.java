3f1ec7e9334040761eb631105b930de19720836b
Maxine Afable <maxineafable1@gmail.com>
2025-12-27 14:37:02 +0800

    fix cors backend

--- backend/src/main/java/com/liftts/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/liftts/backend/config/SecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -29,11 +36,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                                .requestMatchers("/api/v1/auth/**").permitAll()
+//                                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                              .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -57,5 +66,19 @@ public class SecurityConfig {
     @Bean
     public Resend resend(@Value("${resend.api.key}") String key) {
         return new Resend(key);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
--- backend/src/main/java/com/liftts/backend/controllers/AuthenticationController.java
+++ backend/src/main/java/com/liftts/backend/controllers/AuthenticationController.java
@@ -1,6 +1,5 @@
 package com.liftts.backend.controllers;
 
-import com.liftts.backend.domain.dtos.AuthenticationResponse;
 import com.liftts.backend.domain.dtos.LoginRequest;
 import com.liftts.backend.domain.dtos.LoginResponse;
 import com.liftts.backend.domain.entities.User;
@@ -8,13 +7,17 @@ import com.liftts.backend.domain.entities.VerificationToken;
 import com.liftts.backend.repositories.VerificationTokenRepository;
 import com.liftts.backend.services.AuthenticationService;
 import com.liftts.backend.services.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -28,46 +31,64 @@ public class AuthenticationController {
     private final UserDetailsService userDetailsService;
 
     @PostMapping(path = "/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest loginRequest,
+            HttpServletResponse response
+    ) {
         Optional<UserDetails> userDetails = authenticationService.login(
                 loginRequest.getEmail(),
                 loginRequest.getPassword()
         );
         if (userDetails.isPresent()) {
-            String token = authenticationService.generateToken(userDetails.get());
+            String jwtToken = authenticationService.generateToken(userDetails.get());
 
-            AuthenticationResponse authenticationResponse = AuthenticationResponse.builder()
-                    .token(token)
-                    .expiresIn(86400)
+            ResponseCookie cookie = ResponseCookie.from("liftts-session", jwtToken)
+                    .httpOnly(true)
+                    .secure(false) // todo change in prod to true
+                    .sameSite("Lax")
+                    .path("/")
+//                .domain("http://localhost:3000")
+                    .maxAge(Duration.ofMinutes(60)) // todo change in prod
                     .build();
 
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
             LoginResponse loginResponse = LoginResponse.builder()
-                    .authenticationResponse(authenticationResponse)
+                    .authenticationResponse(null)
                     .message(null)
                     .build();
 
             return ResponseEntity.ok(loginResponse);
-        } else {
-            LoginResponse loginResponse = LoginResponse.builder()
-                    .authenticationResponse(null)
-                    .message("Verification link has been resent")
-                    .build();
-            return ResponseEntity.ok(loginResponse);
         }
+
+        LoginResponse loginResponse = LoginResponse.builder()
+                .authenticationResponse(null)
+                .message("Verification link has been resent")
+                .build();
+
+        return ResponseEntity.ok(loginResponse);
     }
 
     @PostMapping(path = "/signup")
-    public ResponseEntity<String> signup(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> signup(@RequestBody LoginRequest loginRequest) {
         authenticationService.signup(
                 loginRequest.getEmail(),
                 loginRequest.getPassword()
         );
 
-        return new ResponseEntity<>("Verification link has been sent", HttpStatus.CREATED);
+        LoginResponse loginResponse = LoginResponse.builder()
+                .authenticationResponse(null)
+                .message("Verification link has been sent")
+                .build();
+
+        return new ResponseEntity<>(loginResponse, HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/verify")
-    public ResponseEntity<AuthenticationResponse> confirmEmailVerification(@RequestParam String token) {
+    public ResponseEntity<Void> confirmEmailVerification(
+            @RequestParam String token,
+            HttpServletResponse response
+    ) {
         VerificationToken verificationToken = verificationTokenRepository.findByToken(token).orElseThrow(
                 () -> new IllegalArgumentException("Invalid token provided")
         );
@@ -87,11 +108,22 @@ public class AuthenticationController {
 
         String jwtToken = authenticationService.generateToken(userDetails);
 
-        AuthenticationResponse authenticationResponse = AuthenticationResponse.builder()
-                .token(jwtToken)
-                .expiresIn(86400)
+        ResponseCookie cookie = ResponseCookie.from("liftts-session", jwtToken)
+                .httpOnly(true)
+                .secure(false) // todo change in prod to true
+                .sameSite("Lax")
+                .path("/")
+//                .domain("http://localhost:3000")
+                .maxAge(Duration.ofMinutes(60)) // todo change in prod
                 .build();
 
-        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        response.setHeader(
+                HttpHeaders.LOCATION,
+                "http://localhost:3000/dashboard"
+        );
+
+        return new ResponseEntity<>(HttpStatus.FOUND);
     }
 }
